0eb73631f5daa32dbc5b9e3b7cf90f36bff6f787
mdev9 <devriesmarijn05@gmail.com>
2024-04-29 18:38:11 +0200

    working on minimap

--- srcs/minimap.c
+++ srcs/minimap.c
@@ -6,7 +6,7 @@
 /*   By: marde-vr <marde-vr@42angouleme.fr>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/29 15:27:46 by marde-vr          #+#    #+#             */
-/*   Updated: 2024/04/29 17:22:24 by marde-vr         ###   ########.fr       */
+/*   Updated: 2024/04/29 18:38:04 by marde-vr         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -37,18 +37,23 @@ void	set_minimap(t_game *game)
 	int i;
 	int j;
 
-	x = game->player->x - 4;
-	y = game->player->y - 4;
 	i = 0;
-	while (game->map[y] && i < 9)
+	y = game->player->y - 4;
+	if (y < 0)
+		y = 0;
+	while (y < game->map_size && game->map[y] && i < 9)
 	{
 		j = 0;
-		x = 0;
-		while (game->map[y][x] && j < 9)
+		x = game->player->x - 4;
+		if (x < 0)
+			x = 0;
+		while (x < (int)ft_strlen(game->map[y]) && game->map[y][x] && j < 9)
 		{
 			if (game->map[y][x] == '0'
 					|| char_is_spawn_pos(game->map[y][x]))
 				put_tile(game, j, i, 0x22FFFFFF);
+			else if (j == game->player->x && i == game->player->y)
+				put_tile(game, j, i, 0xFF00FF00);
 			else 
 				put_tile(game, j, i, 0xFF00A300);
 			x++;

ff26cb1591f612b7339c8646145849bd1b2f8654
mdev9 <devriesmarijn05@gmail.com>
2024-05-01 11:11:49 +0200

    fixed minimap norme

--- srcs/minimap.c
+++ srcs/minimap.c
@@ -6,7 +6,7 @@
 /*   By: marde-vr <marde-vr@42angouleme.fr>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/29 15:27:46 by marde-vr          #+#    #+#             */
-/*   Updated: 2024/05/01 11:05:41 by marde-vr         ###   ########.fr       */
+/*   Updated: 2024/05/01 11:11:41 by marde-vr         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,7 +23,8 @@ void	put_tile(t_game *game, int x, int y, unsigned int color)
 		j = 0;
 		while (j < 32)
 		{
-			mlx_pixel_put(game->mlx, game->mlx_win, x * 32 + j, y * 32 + i, color);
+			mlx_pixel_put(game->mlx, game->mlx_win,
+				x * 32 + j, y * 32 + i, color);
 			j++;
 		}
 		i++;
@@ -41,7 +42,8 @@ void	put_mini_player(t_game *game, int x, int y)
 		j = 8;
 		while (j < 24)
 		{
-			mlx_pixel_put(game->mlx, game->mlx_win, x * 32 + j, y * 32 + i, 0xFF34D5EB);
+			mlx_pixel_put(game->mlx, game->mlx_win,
+				x * 32 + j, y * 32 + i, 0xFF34D5EB);
 			j++;
 		}
 		i++;
@@ -52,31 +54,27 @@ void	set_minimap(t_game *game)
 {
 	double	x;
 	double	y;
-	double	i;
-	double	j;
+	int		i;
+	int		j;
 
-	i = 0;
-	y = game->player->y - 4;
-	while (y < game->map_size && i < 9)
+	i = -1;
+	y = game->player->y - 5;
+	while (++y < game->map_size && ++i < 9)
 	{
-		j = 0;
-		x = game->player->x - 4;
-		while (j < 9)
+		j = -1;
+		x = game->player->x - 5;
+		while (++j < 9)
 		{
-			if (x >= 0 && y >= 0 && game->map[(int)y] && x < (double)ft_strlen(game->map[(int)y]))
+			if (++x >= 0 && y >= 0 && game->map[(int)y]
+				&& x < (double)ft_strlen(game->map[(int)y]))
 			{
-				ft_printf("%d\n", (int)i);
 				if (game->map[(int)y][(int)x] == '0'
 					|| char_is_spawn_pos(game->map[(int)y][(int)x]))
 					put_tile(game, j, i, 0x22FFFFFF);
-				else 
+				else
 					put_tile(game, j, i, 0xFF00A300);
 			}
-			x++;
-			j++;
 		}
-		y++;
-		i++;
 	}
 	put_mini_player(game, 4, 4);
 }
